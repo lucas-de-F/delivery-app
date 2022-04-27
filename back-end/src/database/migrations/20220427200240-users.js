c5fe8ff729606136e5714528bf0d9a0d077e5a12
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-27 17:07:13 -0300

    feat: 🎸 add user-migration

---
+++ back-end/src/database/migrations/20220427200240-users.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('Users');
+  }
+};
