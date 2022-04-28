63ad58ca9fde04c94055fb7fac495191f13e0304
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-28 16:44:12 -0300

    refactor: 💡 update userService

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,8 +1,7 @@
 const cryptor = require('crypto');
-const jwt = require('../utils/jwt');
 
-const { User } = require('../database/models/index');
-const createError = require('../utils/AppError');
+const { User } = require('../database/models');
+const { AppError, jwt } = require('../utils');
 
 const verifyUserEmail = async (email) => {
   const user = await User.findOne({
@@ -18,13 +17,13 @@ const login = async (email, password) => {
   const result = await verifyUserEmail(email);
 
   if (!result) {
-    throw createError('unauthorized', 'Incorrect email or password');
+    throw AppError('unauthorized', 'Incorrect email or password');
   }
 
   const hashPassword = cryptor.createHash('md5').update(password).digest('hex');
 
   if (hashPassword !== result.password) {
-    throw createError('unauthorized', 'Incorrect email or password');
+    throw AppError('unauthorized', 'Incorrect email or password');
   }
 
   const { role } = result;
