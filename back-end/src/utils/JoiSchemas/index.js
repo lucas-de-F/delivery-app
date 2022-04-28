6e08e56efbdd021829d826604193b6f662d1c714
felipenevess <felipe012neves@gmail.com>
2022-04-28 18:19:19 -0300

    feat: add register schema

--- back-end/src/utils/JoiSchemas/index.js
+++ back-end/src/utils/JoiSchemas/index.js
@@ -1,7 +1,9 @@
 const validateJoi = require('./validateJoi');
 const loginSchema = require('./loginSchema');
+const registerSchema = require('./registerSchema');
 
 module.exports = {
   validateJoi,
   loginSchema,
+  registerSchema,
 };
