f95c86e887e49677d4e469e5e27ab774c7404500
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-28 16:42:54 -0300

    feat: 🎸 update authController

--- back-end/src/controllers/authController.js
+++ back-end/src/controllers/authController.js
@@ -1,15 +1,11 @@
-const userService = require('../services/userService');
+const { userService } = require('../services');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  try { 
     const result = await userService.login(email, password);
 
     res.status(200).json({ token: result });
-  } catch (err) {
-    console.log(err);
-  }
 };
 
 module.exports = {
