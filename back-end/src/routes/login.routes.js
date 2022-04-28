32694b255ef011b56e9245d5979dee60eea5e4a9
felipenevess <felipe012neves@gmail.com>
2022-04-28 17:44:39 -0300

    feat: adding validation in the route

--- back-end/src/routes/login.routes.js
+++ back-end/src/routes/login.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
 const { authController } = require('../controllers');
+const validadeJoi = require('../utils/JoiSchemas/validateJoi');
+const schema = require('../utils/JoiSchemas/loginSchema');
 
 const loginRoute = Router();
 
-loginRoute.post('/', authController.login);
+loginRoute.post('/', validadeJoi(schema), authController.login);
 
 module.exports = loginRoute;
