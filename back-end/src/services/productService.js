54a3ea48b165c19f22f9a74841202db100dc9469
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-02 16:16:51 -0300

    feat: 🎸 update complete crud on productsService

--- back-end/src/services/productService.js
+++ back-end/src/services/productService.js
@@ -1,11 +1,32 @@
 const { Product } = require('../database/models');
 
-const findAll = async () => {
+const create = async (product) => {
+  const result = await Product.create(product);
+
+  return result;
+};
+
+const read = async () => {
   const result = await Product.findAll();
 
   return result;
 };
 
+const update = async (id, product) => {
+  const result = await Product.update(product, { where: { id } });
+
+  return result;
+};
+
+const remove = async (id) => {
+  const result = await Product.destroy({ where: { id } });
+
+  return result;
+};
+
 module.exports = {
-  findAll,
+  create,
+  read,
+  update,
+  remove,
 };
