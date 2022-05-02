f8d0e3f1cd2d7b47ff67dfa602050c6cc2f778d7
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-02 18:27:19 -0300

    feat: 🎸 install middleware on products

--- back-end/src/middlewares/index.js
+++ back-end/src/middlewares/index.js
@@ -1,7 +1,9 @@
 const domainError = require('./domain.error');
 const validateJoi = require('./validateJoi');
+const authenticator = require('./authenticator');
 
 module.exports = {
+  authenticator,
   domainError,
   validateJoi,
 };
--- back-end/src/routes/product.routes.js
+++ back-end/src/routes/product.routes.js
@@ -3,15 +3,16 @@ const { Router } = require('express');
 const { productController } = require('../controllers');
 const { productSchema } = require('../utils/JoiSchemas');
 const { validateJoi } = require('../middlewares');
+const { authenticator } = require('../middlewares');
 
 const productRoute = Router();
 
-productRoute.post('/', validateJoi(productSchema), productController.create);
+productRoute.post('/', authenticator, validateJoi(productSchema), productController.create);
 
 productRoute.get('/', productController.read);
 
-productRoute.put('/:id', validateJoi(productSchema), productController.update);
+productRoute.put('/:id', authenticator, validateJoi(productSchema), productController.update);
 
-productRoute.delete('/:id', validateJoi(productSchema), productController.remove);
+productRoute.delete('/:id', authenticator, validateJoi(productSchema), productController.remove);
 
 module.exports = productRoute;
