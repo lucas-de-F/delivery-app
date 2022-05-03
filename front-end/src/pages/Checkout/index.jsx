0ed46d77f28fe7c66d697729d839e9f8ff9b97ae
lucas-de-f <francalucas937@gmail.com>
2022-05-03 19:05:00 -0300

    feat: checkout page

--- front-end/src/pages/Checkout/index.jsx
+++ front-end/src/pages/Checkout/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import Header from '../../components/Header/index';
+import Header from '../../components/Header';
+import ProductList from '../../components/ProductList';
 
 const Checkout = () => (
   <>
     <Header />
+    <ProductList />
     {/* <LoginForm /> */}
   </>
 );
