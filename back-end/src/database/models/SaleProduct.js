90305b614768f4eb9361d03220dc346aa9880334
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-04 14:27:24 -0300

    feat: 🎸 create saleProduct

--- back-end/src/database/models/SaleProduct.js
+++ back-end/src/database/models/SaleProduct.js
@@ -1,41 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-  const SaleProduct = sequelize.define('SalesProducts', {
+  const SalesProducts = sequelize.define('SalesProducts', {
     quantity: DataTypes.INTEGER,
   }, {
     timestamps: false,
-    tableName: 'sales_products'
+    tableName: 'sales_products',
+    underscored: true,
   });
 
-  // SaleProduct.associate = (models) => {
-  //   SaleProduct.belongsTo(models.Sales, {
-  //     foreinKey: 'sale_id', as: 'sales',
-  //     through: SaleProduct,
-  //     otherKey: 'product_id'
-  //   });
-  // }
+  SalesProducts.associate = (model) => {
+    model.Sales.belongsToMany(
+      model.Product, {
+        as: 'products',
+        through: SalesProducts,
+        foreignKey: 'saleId',
+        otherKey: 'productId',
+      }
+    );
 
-  // SaleProduct.associate = (models) => {
-  //   SaleProduct.belongsTo(models.Product, {
-  //     foreinKey: 'product_id', as: 'products',
-  //     through: SaleProduct,
-  //     otherKey: 'sale_id'
-  //   });
-  // }
+    model.Product.belongsToMany(
+      model.Sales, {
+        as: 'sales',
+        through: SalesProducts,
+        foreignKey: 'productId',
+        otherKey: 'saleId',
+      }
+    );
+  };
 
-  SaleProduct.associate = (models) => {
-    models.Sales.belongsToMany(models.Sales, {
-      as: 'sales',
-      through: SaleProduct,
-      foreignKey: 'sale_id',
-      otherKey: 'product_id',
-    });
-    models.Product.belongsToMany(models.Product, {
-      as: 'products',
-      through: SaleProduct,
-      foreignKey: 'product_id',
-      otherKey: 'sale_id',
-    });
-  }
-
-  return SaleProduct;
+  return SalesProducts;
 };
