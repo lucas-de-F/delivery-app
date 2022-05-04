d9167b22bd43d3c4f986a47801ff997b80057b9f
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-04 16:33:14 -0300

    feat: 🎸 update sales route

--- back-end/src/routes/saleRoute.routes.js
+++ back-end/src/routes/saleRoute.routes.js
@@ -4,14 +4,14 @@ const { saleController } = require('../controllers');
 
 const { saleSchema } = require('../utils/JoiSchemas');
 const { validateJoi } = require('../middlewares');
-// const { authenticator } = require('../middlewares');
+const { authenticator } = require('../middlewares');
 
 const saleRoute = Router();
 
-saleRoute.post('/', validateJoi(saleSchema), saleController.create);
+saleRoute.post('/', authenticator('customer'), validateJoi(saleSchema), saleController.create);
 
 saleRoute.get('/:id', saleController.read);
 
-saleRoute.put('/:id', saleController.update);
+saleRoute.put('/:id', authenticator('seller'), saleController.update);
 
 module.exports = saleRoute;
