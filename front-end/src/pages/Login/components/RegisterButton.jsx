582e005c020a4bda814abcd921f52c36c7addad8
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-05-04 18:45:25 -0300

    fix: lint error and removing material ui from register components

--- front-end/src/pages/Login/components/RegisterButton.jsx
+++ front-end/src/pages/Login/components/RegisterButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-const RegisterButton = (name) => {
+const RegisterButton = () => {
   const navigate = useNavigate();
   return (
     <Button
