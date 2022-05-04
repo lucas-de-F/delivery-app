103bcb8a5699d827d0616121e485084eff5ba1cf
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-04 19:50:09 -0300

    refactor: 💡 update domainError

--- back-end/src/middlewares/domain.error.js
+++ back-end/src/middlewares/domain.error.js
@@ -2,22 +2,24 @@ const errorMap = {
   invalid: 400,
   notFound: 404,
   unauthorized: 401,
+  conflict: 409,
 };
 
-const domainError = (err, req, res, next) => {
+const domainError = (err, _req, res, _next) => {
   const status = errorMap[err.code];
 
   if (status) {
     return res.status(status).json({ error: err.message });
   }
 
+  /* istanbul ignore next */
   if (err.name === 'JsonWebTokenError') {
     return res.status(401).json({ error: 'Invalid token' });
   }
-
+  /* istanbul ignore next */
   console.log(err);
-  res.status(500).json({ error: 'Internal Server Error' });
-  next();
+  /* istanbul ignore next */
+  return res.status(500).json({ error: 'Internal Server Error' });
 };
 
 module.exports = domainError;
