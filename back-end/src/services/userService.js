42cde98a4a1a94f445b3759bbd0b993dd7231280
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-11 15:24:28 -0300

    feat: 🎸 add getSellers on Service

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -51,7 +51,10 @@ const createUser = async ({ name, email, password }) => {
   return token;
 };
 
+const getSellers = async () => User.findAll({ where: { role: 'seller' } });
+
 module.exports = {
   login,
   createUser,
+  getSellers,
 };
