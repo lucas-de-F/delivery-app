c36f3563e07722618cf3aa3a010d011d1c3c8b6b
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-13 17:31:14 -0300

    feat: 🎸 update detailsStatus

--- front-end/src/pages/DetailsOrder/components/DetailsStatus.jsx
+++ front-end/src/pages/DetailsOrder/components/DetailsStatus.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-// import generateId from '../../../utils/generateId';
+import generateId from '../../../utils/generateId';
 
 const utc = require('dayjs/plugin/utc');
 const dayjs = require('dayjs');
 
 dayjs.extend(utc);
+
+const SELLERNAME = 'customer_order_details__element-order-details-label-seller-name';
+const ORDERDATE = 'customer_order_details__element-order-details-label-order-date';
+const DELSTAT = 'customer_order_details__element-order-details-label-delivery-status';
+const DELCHECK = 'customer_order_details__button-delivery-check';
 
 const DetailsStatus = () => {
   const [orderDetail, setOrderDetail] = useState(null);
@@ -14,52 +19,58 @@ const DetailsStatus = () => {
   const pageId = Number(window.location.pathname.split('/')[3]);
   const { orders } = useSelector((state) => state.OrderSlice);
   const { sellers } = useSelector((state) => state.SellersSlice);
+
   useEffect(() => {
-    const findOrder = orders.find((order) => order.id === pageId);
-    const findSeller = sellers.find((seller) => seller.id === findOrder.sellerId);
-    setSellerDetail(findSeller);
-    setOrderDetail(findOrder);
+    if (orders && orders.length > 0) {
+      const findOrder = orders.find((order) => order.id === pageId);
+      const findSeller = sellers.find((seller) => seller.id === findOrder.sellerId);
+
+      setSellerDetail(findSeller);
+      setOrderDetail(findOrder);
+    }
   }, [orders, pageId, sellers, orderDetail]);
 
-  return ((orderDetail !== null && sellerDetail !== null) && (
-    <div style={ { width: 500 } }>
-      Detalhes do pedido
-      <div>
-        <ul
-          style={ { display: 'flex', justifyContent: 'space-evenly',
-          } }
-        >
-          <li
-            data-testid="customer_order_details__element-order-details-label-order-id"
-          >
-            {/* { `Pedido: ${generateId(orderDetail.id)}` } */}
-          </li>
-          <li
-            data-testid=" customer_order_details__element-order-details-label-seller-name"
-          >
-            { `P. Vendedora: ${sellerDetail.name}` }
-          </li>
-          <li
-            data-testid="customer_order_details__element-order-details-label-order-date"
-          >
-            {/* { `${dayjs.utc(orderDetail.saleDate).format('DD/MM/YYYY')}` } */}
-          </li>
-          <li
-            data-testid="
-        customer_order_details__element-order-details-label-delivery-status"
-          >
-            {/* { orderDetail.status } */}
-          </li>
-          <button
-            type="button"
-            data-testid="customer_order_details__button-delivery-check"
-          >
-            Marcar como entregue
-          </button>
-        </ul>
-      </div>
+  return (
+    <div>
+      { orderDetail && (
+        <div style={ { width: 500 } }>
+          Detalhes do pedido
+          <div>
+            <ul
+              style={ { display: 'flex', justifyContent: 'space-evenly',
+              } }
+            >
+              <li
+                data-testid="customer_order_details__element-order-details-label-order-id"
+              >
+                { `Pedido: ${generateId(orderDetail.id)}` }
+              </li>
+              <li
+                data-testid={ SELLERNAME }
+              >
+                { `P. Vendedora: ${sellerDetail.name}` }
+              </li>
+              <li
+                data-testid={ ORDERDATE }
+              >
+                { `${dayjs.utc(orderDetail.saleDate).format('DD/MM/YYYY')}` }
+              </li>
+              <li
+                data-testid={ DELSTAT }
+              >
+                { orderDetail.status }
+              </li>
+              <button
+                type="button"
+                data-testid={ DELCHECK }
+              >
+                Marcar como entregue
+              </button>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
-  )
   );
 };
 
--- front-end/src/pages/DetailsOrder/components/ProductList.jsx
+++ front-end/src/pages/DetailsOrder/components/ProductList.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 // import { useSelector } from 'react-redux';
 
-const ProductList = () => (
-  <>
-    aaaa
-    {/* {
+const ProductList = () => {
+  useEffect(() => {
+    if (orders && orders.length > 0) {
+      const findOrder = orders.find((order) => order.id === pageId);
+      const findSeller = sellers.find((seller) => seller.id === findOrder.sellerId);
+
+      setSellerDetail(findSeller);
+      setOrderDetail(findOrder);
+    }
+  }, []);
+  return (
+    <>
+      {
         products.map((product) => (
           <ol key={ product.id }>
             <ul>
@@ -15,7 +24,8 @@ const ProductList = () => (
             </ul>
           </ol>
         ))
-      } */}
-  </>
-);
+      }
+    </>
+  );
+};
 export default ProductList;
