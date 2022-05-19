27f78836b456709c7bb8630504932b9579a7b5ad
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-19 20:31:41 -0300

    feat: 🎸 create admin Page

---
+++ front-end/src/pages/Admin/AdminPage/index.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { OrderRequestThunk } from '../../../redux/requestThunks/orderRequest';
+
+import { Header } from '../../../components';
+import { RegisterForm } from './components';
+
+const AdminPage = () => {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => state.UserSlice);
+  const { token } = useSelector((state) => state.UserSlice.auth);
+
+  useEffect(() => {
+    dispatch(OrderRequestThunk({ auth, token }));
+  }, [auth, dispatch, token]);
+
+  return (
+    <>
+      <Header />
+      <section>
+        <RegisterForm />
+      </section>
+    </>
+  );
+}
+
+export default AdminPage;
