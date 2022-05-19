282892ff0962979eb30e4f47706c5d9f9e374c81
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-19 20:30:45 -0300

    feat: 🎸 create Register Schema

---
+++ front-end/src/pages/Admin/AdminPage/components/RegisterSchema.jsx
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+const minPwd = 6;
+const minName = 12;
+
+const RegisterSchema = Joi.object({
+  name: Joi.string().min(minName).required(),
+  email: Joi.string().email({ tlds: { allow: false } }).required(),
+  role: Joi.string().valid('seller', 'customer').required(),
+  password: Joi.string().min(minPwd).required(),
+});
+
+export default RegisterSchema;
