79245c992d72debd57be5f752d6624e528ebb7b9
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-19 02:24:42 -0300

    refactor: 💡 update ProductList

--- front-end/src/pages/DetailsOrder/components/ProductList.jsx
+++ front-end/src/pages/DetailsOrder/components/ProductList.jsx
@@ -1,53 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { dataTestId } from '../../../utils';
 
-const ProductList = () => {
-  const [products, setProducts] = useState(null);
-  const pageId = Number(window.location.pathname.split('/')[3]);
-  const { orders } = useSelector((state) => state.OrderSlice);
+const ProductList = ({ productsList }) => (
+  <>
+    {(
+      productsList.map((product, key) => (
+        <ol key={ key }>
+          <ul
+            data-testid={ `${dataTestId.id41}${key}` }
+          >
+            { product.id }
+          </ul>
+          <ul
+            data-testid={ `${dataTestId.id42}${key}` }
+          >
+            { product.name }
+          </ul>
+          <ul
+            data-testid={ `${dataTestId.id43}${key}` }
+          >
+            { product.SalesProducts.quantity }
+          </ul>
+          <ul
+            data-testid={ `${dataTestId.id46}${key}` }
+          >
+            { product.price }
+          </ul>
+          <ul
+            data-testid={ `${dataTestId.id44}${key}` }
+          >
+            { (product.SalesProducts.quantity * product.price).toFixed(2) }
+          </ul>
+        </ol>
+      ))
+    )}
+  </>
+);
 
-  useEffect(() => {
-    if (orders && orders.length !== 0) {
-      const findOrder = orders.find((order) => order.id === pageId);
-      return setProducts(findOrder.products);
-    }
-  }, [orders, pageId]);
-
-  return (
-    <div>
-      { products && (
-        products.map((product, key) => (
-          <ol key={ key }>
-            <ul
-              data-testid={ `${dataTestId.id41}${key}` }
-            >
-              { product.id }
-            </ul>
-            <ul
-              data-testid={ `${dataTestId.id42}${key}` }
-            >
-              { product.name }
-            </ul>
-            <ul
-              data-testid={ `${dataTestId.id43}${key}` }
-            >
-              { product.SalesProducts.quantity }
-            </ul>
-            <ul
-              data-testid={ `${dataTestId.id46}${key}` }
-            >
-              { product.price }
-            </ul>
-            <ul
-              data-testid={ `${dataTestId.id44}${key}` }
-            >
-              { (product.SalesProducts.quantity * product.price).toFixed(2) }
-            </ul>
-          </ol>
-        ))
-      ) }
-    </div>
-  );
+ProductList.propTypes = {
+  productsList: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    price: PropTypes.string,
+    SalesProducts: PropTypes.shape({
+      quantity: PropTypes.number,
+    }),
+  })).isRequired,
 };
+
 export default ProductList;
