6de1d64891e0e3ae0e06d61034b943115a6f9a89
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-19 22:45:00 -0300

    feat: 🎸 update Seller Details

---
+++ front-end/src/pages/Seller/SellerDetails/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as SectionDetails } from './SectionDetails';
+export { default as ProductsList } from './ProductsList';
+export { default as TotalPrice } from './TotalPrice';
--- front-end/src/pages/Seller/SellerDetails/index.jsx
+++ front-end/src/pages/Seller/SellerDetails/index.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../../components/Header';
 import { OrderRequestThunk } from '../../../redux/requestThunks/orderRequest';
 
-import SectionDetails from './components/SectionDetails';
+import { ProductsList, SectionDetails, TotalPrice } from './components';
 
 function SellerDetails() {
   const dispatch = useDispatch();
+  const [orderDetail, setOrderDetail] = useState(null);
+  const pageId = Number(window.location.pathname.split('/')[3]);
+
+  const { orders } = useSelector((state) => state.OrderSlice);
   const { auth } = useSelector((state) => state.UserSlice);
   const { token } = JSON.parse(localStorage.getItem('user'));
 
@@ -14,10 +18,31 @@ function SellerDetails() {
     dispatch(OrderRequestThunk({ auth, token }));
   }, [auth, dispatch, token]);
 
+  useEffect(() => {
+    if (orders && orders.length !== 0) {
+      const findOrder = orders.find((order) => order.id === pageId);
+      return setOrderDetail(findOrder);
+    }
+  }, [orderDetail, orders, pageId]);
+
   return (
     <>
       <Header />
-      <SectionDetails />
+      {
+        orderDetail && (
+          <section>
+            <SectionDetails
+              orderDetail={ {
+                id: orderDetail.id,
+                date: orderDetail.saleDate,
+                status: orderDetail.status,
+              } }
+            />
+            <ProductsList products={ orderDetail.products } />
+            <TotalPrice value={ orderDetail.totalPrice } />
+          </section>
+        )
+      }
     </>
   );
 }
