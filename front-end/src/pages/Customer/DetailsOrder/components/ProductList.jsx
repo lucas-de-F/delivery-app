13f02492e122d4456707936d59640e8a73998cc5
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-19 22:43:48 -0300

    refactor: 💡 ProductList of details order

--- front-end/src/pages/Customer/DetailsOrder/components/ProductList.jsx
+++ front-end/src/pages/Customer/DetailsOrder/components/ProductList.jsx
@@ -4,39 +4,40 @@ import PropTypes from 'prop-types';
 import { dataTestId } from '../../../../utils';
 
 const ProductList = ({ productsList }) => (
-  <>
-    {(
-      productsList.map((product, key) => (
-        <ol key={ key }>
-          <ul
-            data-testid={ `${dataTestId.id41}${key}` }
-          >
-            { product.id }
-          </ul>
-          <ul
-            data-testid={ `${dataTestId.id42}${key}` }
-          >
-            { product.name }
-          </ul>
-          <ul
-            data-testid={ `${dataTestId.id43}${key}` }
-          >
-            { product.SalesProducts.quantity }
-          </ul>
-          <ul
-            data-testid={ `${dataTestId.id46}${key}` }
-          >
-            { product.price }
-          </ul>
-          <ul
-            data-testid={ `${dataTestId.id44}${key}` }
-          >
-            { (product.SalesProducts.quantity * product.price).toFixed(2) }
-          </ul>
-        </ol>
-      ))
-    )}
-  </>
+  <table>
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Descrição</th>
+        <th>Quantidade</th>
+        <th>Valor Unitário</th>
+        <th>Sub-total</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        productsList.map((product, key) => (
+          <tr key={ `key-${key}` }>
+            <td data-testid={ `${dataTestId.id41}${key}` }>
+              { product.id }
+            </td>
+            <td data-testid={ `${dataTestId.id42}${key}` }>
+              { product.name }
+            </td>
+            <td data-testid={ `${dataTestId.id43}${key}` }>
+              { product.SalesProducts.quantity }
+            </td>
+            <td data-testid={ `${dataTestId.id46}${key}` }>
+              { product.price }
+            </td>
+            <td data-testid={ `${dataTestId.id44}${key}` }>
+              { (product.SalesProducts.quantity * product.price).toFixed(2) }
+            </td>
+          </tr>
+        ))
+      }
+    </tbody>
+  </table>
 );
 
 ProductList.propTypes = {
