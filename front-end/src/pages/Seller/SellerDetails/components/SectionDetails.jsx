acd894f512bca33a5fd816123cd73c8870aa31a9
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-19 22:44:31 -0300

    feat: 🎸 update Section Details

--- front-end/src/pages/Seller/SellerDetails/components/SectionDetails.jsx
+++ front-end/src/pages/Seller/SellerDetails/components/SectionDetails.jsx
@@ -1,88 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import dayjs from 'dayjs/';
+import PropTypes from 'prop-types';
 
-import { useSelector } from 'react-redux';
 import { dataTestId, generateId } from '../../../../utils';
 import ButtonStatus from './ButtonStatus';
 
-const SectionDetails = () => {
-  const pageId = Number(window.location.pathname.split('/')[3]);
-  const [orderDetail, setOrderDetail] = useState(null);
-
-  const { orders } = useSelector((state) => state.OrderSlice);
-
-  useEffect(() => {
-    if (orders && orders.length !== 0) {
-      const findOrder = orders.find((order) => order.id === pageId);
-      return setOrderDetail(findOrder);
-    }
-  }, [orderDetail, orders, pageId]);
+const SectionDetails = ({ orderDetail }) => {
+  const { id, date, status } = orderDetail;
 
   return (
     <>
       <h3>Detalhe do Pedido</h3>
-      {
-        orderDetail && (
-          <div>
-            <div>
-              <p data-testid={ dataTestId.id53 }>
-                { `PEDIDO ${generateId(orderDetail.id)}` }
-              </p>
-              <p data-testid={ dataTestId.id55 }>
-                {dayjs(orderDetail.saleDate).locale('pt-br').format('DD/MM/YYYY')}
-              </p>
-              <p data-testid={ dataTestId.id54 }>
-                { orderDetail.status }
-              </p>
-              <ButtonStatus
-                dataId={ dataTestId.id56 }
-                title="PREPARAR PEDIDO"
-                status={ orderDetail.status }
-                id={ orderDetail.id }
-              />
-              <ButtonStatus
-                dataId={ dataTestId.id57 }
-                title="SAIU PARA ENTREGA"
-                status={ orderDetail.status }
-                id={ orderDetail.id }
-              />
-              {
-                pageId
-              && orderDetail.products.map((item, i) => (
-                <div key={ `key-${i}` }>
-                  <p data-testid={ `${dataTestId.id58}${item.id}` }>
-                    { `Item ${item.id}` }
-                  </p>
-                  <p data-testid={ `${dataTestId.id59}${item.id}` }>
-                    { `Descrição ${item.name}` }
-                  </p>
-                  <p data-testid={ `${dataTestId.id60}${item.id}` }>
-                    { `Quantidade ${item.SalesProducts.quantity}` }
-                  </p>
-                  <p data-testid={ `${dataTestId.id61}${item.id}` }>
-                    {`Valor Unitário ${item.price}` }
-                  </p>
-                  <p data-testid={ `${dataTestId.id62}${item.id}` }>
-                    { `Sub-total ${(
-                      Number(item.price) * item.SalesProducts.quantity
-                    ).toFixed(2)}` }
-                  </p>
-                </div>
-              ))
-              }
-              {
-                pageId && (
-                  <h2 data-testid={ dataTestId.id63 }>
-                    {orderDetail.totalPrice.toString().replace('.', ',')}
-                  </h2>
-                )
-              }
-            </div>
-          </div>
-        )
-      }
+      <div
+        style={ { display: 'flex',
+          'justify-content': 'space-between',
+          'align-items': 'center',
+        } }
+      >
+        <p data-testid={ dataTestId.id53 }>
+          { `PEDIDO ${generateId(id)}` }
+        </p>
+        <p data-testid={ dataTestId.id55 }>
+          {dayjs(date).locale('pt-br').format('DD/MM/YYYY')}
+        </p>
+        <p data-testid={ dataTestId.id54 }>
+          { status }
+        </p>
+        <ButtonStatus
+          dataId={ dataTestId.id56 }
+          title="PREPARAR PEDIDO"
+          status={ status }
+          id={ orderDetail.id }
+        />
+        <ButtonStatus
+          dataId={ dataTestId.id57 }
+          title="SAIU PARA ENTREGA"
+          status={ status }
+          id={ orderDetail.id }
+        />
+      </div>
     </>
   );
+};
+
+SectionDetails.propTypes = {
+  orderDetail: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default SectionDetails;
