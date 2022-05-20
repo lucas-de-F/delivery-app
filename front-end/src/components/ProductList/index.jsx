5687c5200fc0f19efa15ddcea8838b494a3e399b
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-19 22:43:15 -0300

    refactor: 💡 product list of components

--- front-end/src/components/ProductList/index.jsx
+++ front-end/src/components/ProductList/index.jsx
@@ -11,55 +11,68 @@ const ProductList = () => {
   useEffect(() => {
     dispatch(setCartByInput(cart));
   }, [cart, dispatch]);
-  const toFixedNumber = 3;
+
   return (
     <div style={ { width: 500 } }>
-      Finalizar produto
-      <ol>
-        { !cart ? 'carrinho vazio' : Object.keys(cart).map((payload, key) => (
-          <li
-            key={ key }
-            style={ { display: 'flex', justifyContent: 'space-between', margin: 10 } }
-          >
-            <div
-              data-testid={
-                `${dataTestId.id22}${key}`
+      Finalizar Pedido
+      { Object.keys(cart).length === 0 ? <h4>Carrinho Vazio</h4>
+        : (
+          <table>
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Descrição</th>
+                <th>Quantidade</th>
+                <th>Valor Unitário</th>
+                <th>Sub-total</th>
+                <th>Remover Item</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                Object.keys(cart).map((payload, key) => (
+                  <tr key={ `key-${key}` }>
+                    <td data-testid={ `${dataTestId.id22}${key}` }>
+                      { key + 1 }
+                    </td>
+                    <td data-testid={ `${dataTestId.id23}${key}` }>
+                      { payload }
+                    </td>
+                    <td
+                      data-testid={ `${dataTestId.id24}${key}` }
+                    >
+                      {cart[payload].quantity}
+                    </td>
+                    <td data-testid={ `${dataTestId.id25}${key}` }>
+                      {Number(cart[payload].price)
+                        .toFixed(2)
+                        .toString()
+                        .replace('.', ',')}
+                    </td>
+                    <td data-testid={ `${dataTestId.id26}${key}` }>
+                      {(cart[payload].quantity * cart[payload].price)
+                        .toFixed(2)
+                        .toString()
+                        .replace('.', ',')}
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        data-testid={ `${dataTestId.id27}${key}` }
+                        onClick={ () => dispatch(removeCart({
+                          name: payload,
+                          removeAll: true,
+                        })) }
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))
               }
-            >
-              {key + 1}
-            </div>
-            <div data-testid={ `${dataTestId.id23}${key}` }>
-              {payload}
-            </div>
-            <div
-              data-testid={
-                `${dataTestId.id24}${key}`
-              }
-            >
-              {cart[payload].quantity}
-            </div>
-            <div
-              data-testid={ `${dataTestId.id25}${key}` }
-            >
-              {Number(cart[payload].price)
-                .toFixed(toFixedNumber).toString().replace('.', ',')}
-            </div>
-            <div
-              data-testid={ `${dataTestId.id26}${key}` }
-            >
-              {(cart[payload].quantity * cart[payload].price)
-                .toFixed(toFixedNumber).toString().replace('.', ',')}
-            </div>
-            <button
-              type="button"
-              data-testid={ `${dataTestId.id27}${key}` }
-              onClick={ () => dispatch(removeCart({ name: payload, removeAll: true })) }
-            >
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
+            </tbody>
+          </table>
+        )}
     </div>
   );
 };
